fdcfa6b0a55926644da6d0e09c079b1c2a86a0c9
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-05-27 15:24:27 +0000

    a

---
+++ atv_4.js
@@ -0,0 +1,23 @@
+// Atividade 4
+
+// 2 Variáveis
+// Operador Aritmético (+,-,*,/)
+// Operador Lógico (&&, ||)
+// Estrutura Condicional (if, else if, else)
+// Exibição (console.log())
+
+let nome = "MoonBe"
+let ano_nascimento = 2012
+
+let idade = 2025 - ano_nascimento
+
+if(nome == "MoonBe" && ano_nascimento == 2012){
+    console.log("-------------")
+    console.log("Nome: ", nome)
+    console.log("Idade: ", idade)
+    console.log("-------------")
+    console.log("Seja bem-vinda, Be.")
+}
+else{
+    console.log("Da o fora daqui >:c")
+}
