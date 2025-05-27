48379d48cc461c1b3a2b07485c5df44605d582e5
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-05-27 15:30:10 +0000

    detalhadamento do código

--- atv_4.js
+++ atv_4.js
@@ -6,18 +6,18 @@
 // Estrutura Condicional (if, else if, else)
 // Exibição (console.log())
 
-let nome = "MoonBe"
-let ano_nascimento = 2011
+let nome = "MoonBe" // Variável 1
+let ano_nascimento = 2011 // Variável 2
 
-let idade = 2025 - ano_nascimento
+let idade = 2025 - ano_nascimento // Variável com Operador Aritmético (subtração)
 
-if(nome == "MoonBe" && ano_nascimento == 2011){
+if(nome == "MoonBe" && ano_nascimento == 2011){ // Condicional SE (if) o nome for MoonBe E (&&) o ano de nascimento for 2011:
     console.log("-------------")
     console.log("Nome: ", nome)
     console.log("Idade: ", idade)
     console.log("-------------")
-    console.log("Seja bem-vinda, Be.")
+    console.log("Seja bem-vinda, Be.") // Console logs pra mostrar uma ficha de boas vindas.
 }
 else{
-    console.log("Da o fora daqui >:c")
+    console.log("Da o fora daqui >:c") // Fim da condicional, SE NÃO (else) [...], mensagem pra te expulsar.
 }
